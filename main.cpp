b734a6f953777a7ddb6bca2dc973420a4133da79
LeoWu979 <leo09287@gmail.com>
2021-03-30 07:14:17 -0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include "mbed.h"
 #include "uLCD_4DGL.h"
-
+using namespace std::chrono;
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 DigitalOut btn_confirm(D13);
 DigitalOut btn_down(D12);
 DigitalOut btn_up(D11);
+AnalogOut  aout(PA_4);
+AnalogIn Ain(A0);
+
+Timer t;
 
 int main()
 {
+
+    
     // basic printf demo = 16 by 18 characters on screen
     uLCD.locate(0,0);
     uLCD.printf("Select Frequency\n"); //Default Green on black text
@@ -18,25 +24,25 @@ int main()
     uLCD.text_height(2);
     uLCD.color(BLUE);
     uLCD.locate(2,1);
-    uLCD.printf("200Hz");    
+    uLCD.printf("100Hz");    
 
     uLCD.text_width(2);
     uLCD.text_height(2);
     uLCD.color(BLUE);
     uLCD.locate(2,3);
-    uLCD.printf("400Hz");  
+    uLCD.printf("200Hz");  
 
     uLCD.text_width(2);
     uLCD.text_height(2);
     uLCD.color(BLUE);
     uLCD.locate(2,5);
-    uLCD.printf("600Hz");  
+    uLCD.printf("400Hz");  
 
     uLCD.text_width(2);
     uLCD.text_height(2);
     uLCD.color(BLUE);
     uLCD.locate(2,7);
-    uLCD.printf("900Hz");  
+    uLCD.printf("500Hz");  
 
     int mode = 0;
 
@@ -120,5 +126,65 @@ int main()
         if (btn_confirm) break;
 
     }
+
+    int delay_time = 0, cnt = 0, k = 0;
+
+    if (mode == 0) delay_time = 85;
+    if (mode == 1) delay_time = 33;
+    if (mode == 2) delay_time = 10;
+    if (mode == 3) delay_time = 6;
+
+
+    float bound = 3/3.3f;
+    float step1 = 3/3.3f/40;
+    float step2 = 3/3.3f/60; 
+    float launch_time = 0;
+
+    if (mode < 2) launch_time = 1.0/(100*(mode+1))*2*1000000;
+    if (mode >= 2) launch_time = 1.0/(100*(mode+2))*2*1000000;
+    float ADCdata[200];
+
+    while (1) {
+      // change the voltage on the digital output pin by 0.1 * VCC
+      //  and print what the measured voltage should be (assuming VCC = 3.3v)
+        t.start();
+
+        for (float i = 0.0f; i < bound; i += step1) {
+            aout = i;
+            if (cnt > 20 && cnt < 23 && k < 200) {
+                ADCdata[k] = Ain;
+                k++;
+            } 
+            
+            wait_us(delay_time);
+        }
+//        t.stop();
+//        auto us = t.elapsed_time().count();
+//        printf ("Timer time: %llu us\n", us);
+        for (float i = bound; i > 0.0f; i -= step2) {
+            aout = i;
+            if (cnt > 20 && cnt < 23 && k < 200) {
+                ADCdata[k] = Ain;
+                k++;
+            }             
+            wait_us(delay_time);
+        }
+        t.stop();
+        if (cnt == 0) {
+            auto us = t.elapsed_time().count();
+//            printf ("Timer time: %llu us\n", us);
+//            launch_time = float(us);
+        }
+        if (cnt == 22) {
+            printf("%f\r\n", launch_time);
+            printf("%f\r\n", launch_time);
+            for (k = 0; k < 200; k++)
+                printf("%f\r\n", ADCdata[k]);
+        }
+            
+        
+        cnt++;
+    }
+
     
 }
